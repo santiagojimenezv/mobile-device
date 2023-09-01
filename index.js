b259c50eb44414719ddf86cbf35b3a8cd1f789cf
santiagojimenezv <s.jimenezv10@gmail.com>
2023-08-31 19:42:29 -0500

    Initial state

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const {HOST, PORT, API_PATH, } = require("./variables");
+
+console.log(`${HOST}`);
---
+++ variables.js
@@ -0,0 +1,18 @@
+PORT = 3000
+HOST = 'localhost'
+API_VERSION = 'v1'
+API_PATH = '/api/' + API_VERSION
+DB_HOST = 'cluster0.8y6zqe7.mongodb.net/'
+USERNAME = 'santiagojimenezv'
+PASSWORD = 'X1dRuSWOvG9fg0KI'
+DB_NAME = 'myfirstprojectdb'
+
+module.exports = {
+    PORT,
+    HOST,
+    API_PATH,
+    DB_HOST,
+    USERNAME,
+    PASSWORD,
+    DB_NAME
+}
